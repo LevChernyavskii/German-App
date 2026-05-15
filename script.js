c1b05cfe7a9d980e80843c704e693113c2defcc0
Lev Chernyavskii <charnyavskiylev@gmail.com>
2026-05-15 14:20:37 +0600

    checking how works a link

---
+++ script.js
@@ -0,0 +1,15 @@
+// const day = {
+//     day: 16,
+//     type: "GRAMMAR_DAY",
+//     topic: "Предлоги места (Dativ)",
+//     instruction: "Разбери, как меняются артикли после 'in' и 'auf', когда мы отвечаем на вопрос 'Где?'",
+
+//     videoTime: 20,
+//     grammarTime: 25,
+//     wordsCount: 10,
+// }
+
+// const words = {
+//     1: "текст для импорта",
+//     2: "текст для импорта",
+// }
